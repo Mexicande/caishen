7b7ebe5929226dc59715379da72a64d5faa99820
mexicande <mexicande@hotmail.com>
2018-10-31 18:53:26 +0800

    some

--- app/src/main/java/com/example/expense/DoubleFlower/common/Contacts.java
+++ app/src/main/java/com/example/expense/DoubleFlower/common/Contacts.java
@@ -7,7 +7,7 @@ package com.example.expense.DoubleFlower.common;
 public interface Contacts {
     String LEAN_KEY="1Xractc2sikvNt5m6r9GP7sK";
     String LEAN_ID="9zeHS4F8GAHi97Fkg0p80FYV-gzGzoHsz";
-    String UMENG_KEY="5b88ec49f29d985e2b000077";
+    String UMENG_KEY="5bd94d68f1f5569956000139";
 
     String TOKEN="token";
     String PHONE="phone";
